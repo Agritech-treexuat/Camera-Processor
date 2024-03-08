b8487962286dfa71c3a53d9b08a10e1c36dedae5
Huy0110 <duchuy01102001@gmail.com0>
2024-03-08 14:35:15 +0700

    Update new db (mongo atlas)

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
     MONGO_PORT = os.getenv("MONGO_PORT")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
+    MONGO_URI = os.getenv("MONGO_URI")
+
     WASABI_ACCESS_KEY = os.getenv("WASABI_ACCESS_KEY")
     WASABI_SECRET_KEY = os.getenv("WASABI_SECRET_KEY")
     WASABI_REGION = os.getenv("WASABI_REGION")
@@ -21,6 +23,8 @@ def main():
     WASABI_BUCKET_NAME = os.getenv("WASABI_BUCKET_NAME")
 
     mongo_uri = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
+    if MONGO_URI:
+        mongo_uri = MONGO_URI
     db_name = MONGO_DB_NAME
     frame_skip = 10
 
